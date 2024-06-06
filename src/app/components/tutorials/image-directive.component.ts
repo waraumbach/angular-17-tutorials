e40e6b9c5e238df1cffdbed672e2b7c1bcc8a9c7
Sutin <poly.dev2018@gmail.com>
2024-06-06 22:59:45 +0700

    add form

--- src/app/components/tutorials/image-directive.component.ts
+++ src/app/components/tutorials/image-directive.component.ts
@@ -1,15 +1,16 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-image-directive',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   template: `
     <p>
       image-directive works!
     </p>
+    
   `,
-  styles: ``
 })
 export class ImageDirectiveComponent {
 
--- src/app/components/tutorials/typed-forms.component.ts
+++ src/app/components/tutorials/typed-forms.component.ts
@@ -1,16 +1,37 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-typed-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   template: `
-    <p>
-      typed-forms works!
-    </p>
+    <div>
+  <form [formGroup]="login">
+    username : <input type="text" formControlName="username"> <br />
+    password : <input type="text" formControlName="password"> <br />
+    age : <input type="text" formControlName="age"> <br />
+      {{ login.value | json }}
+    <button (click)="reset()">reset</button>
+  </form>
+    </div>
   `,
-  styles: ``
 })
 export class TypedFormsComponent {
+
+  login = new FormGroup({
+    username: new FormControl<string>('test', { nonNullable: true }),
+    password: new FormControl<string>('23345'),
+    age: new FormControl<number>(20),
+  })
+
+  submit() {
+    const value = this.login.value;
+  }
+
+  reset() {
+    this.login.reset();
+  }
 
 }
